fa534f808c711f4fece283a5a50c0db3a7b9b09d
VIL20776 <vil20776@uvg.edu.gt>
2026-05-09 00:49:42 -0600

    updated deployment workflow

--- deploy.py
+++ deploy.py
@@ -2,11 +2,12 @@ import mlflow
 import mlflow.pytorch
 import os
 
-#Retrieve model version argument from workflow input
+
+model_name = os.getenv("MODEL_NAME")
 model_version = os.getenv("MODEL_VERSION")
 
 #Retrieve the model from MLflow Model Registry
-model_uri = f"models:/SimpleClassifier@{model_version}"  # Use the model version specified in the workflow input
+model_uri = f"models:/{model_name}@{model_version}"  # Use the model version specified in the workflow input
 model = mlflow.pytorch.load_model(model_uri)
 # Save the model to a local directory
 mlflow.pytorch.save_model(model, "model/")
